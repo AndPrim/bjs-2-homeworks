f87596ec93ae87514ba4fcb6dcc8fdd135ad1383
AndPrim <andreyminoss@gmail.com>
2024-03-13 14:44:10 +0500

    OneCommit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d === 0) {
+    arr[0] = -b / (2 * a);
+  } else if (d > 0 ) { 
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  }
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentMonth = percent / 100 / 12;
+  let loanBody = amount - contribution;
+
+  let paySum = loanBody * (percentMonth + (percentMonth / (Math.pow(1 + percentMonth), countMonths)));
+  let totalSum = paySum * countMonths;
+
+  return +(totalSum.toFixed(2));
+}
+
+function testCase(){
+  (calculateTotalMortgage(10, 0, 50000, 12) == 52749.53)? console.log('Первый тест прошел'):
+   console.log('Все плохо');
 }
