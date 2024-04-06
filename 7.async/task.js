2b77e55f7009e2fdbe0172e1cb3988043f8f156d
AndPrim <andreyminoss@gmail.com>
2024-04-06 13:56:04 +0500

    AsyncCommit

---
+++ 7.async/task.js
@@ -0,0 +1,60 @@
+class AlarmClock {
+
+    constructor(){
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback){
+
+        if(!time || !callback){
+            throw new Error("Отсутствуют обязательные аргументы");
+        } else if (this.alarmCollection.find(call => call.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+
+        this.alarmCollection.push({
+            time: time,
+            callback: callback,
+            canCall: true
+        });
+    }
+
+    removeClock(time){
+        this.alarmCollection = this.alarmCollection.filter(call => call.time !== time);
+    }
+
+    getCurrentFormattedTime(){
+        return new Date().toLocaleDateString().slice(0, -3);
+    }
+
+    start(){
+        if(this.intervalId){
+            return;
+        }
+
+        this.intervalId = setInterval( () => {
+			this.alarmCollection.forEach( call => {
+				if (call.time === this.getCurrentFormattedTime() && call.canCall) {
+					call.canCall = false;
+					call.callback();          
+				}
+			})
+		}, 1000);
+    }
+
+     stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+    
+    resetAllCalls() {
+		this.alarmCollection.forEach( call => call.canCall = true);
+	}
+
+    clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
+	
+}
