a90b44ffb9091510672d83bb6ed08668c1814ab4
AndPrim <andreyminoss@gmail.com>
2024-04-10 10:01:19 +0500

    DecoratorsCommit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,48 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
-}
+    let cache = [];
+    function wrapper(...args) {
+      const hash = md5(args);
 
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
+      
+      if (cache.find(item => item.hash === hash)) {
+        return `Из кэша: ${objectInCache.value}`;
+      }
+
+      let result = func(...args);
+      cache.push({ "hash": hash, "value": result });
+
+      if (cache.length > 5) {
+        cache.shift();
+      }
+
+      return `Вычисляем: ${result}`;
+
+    }
+    return wrapper;
+  }
   
-}
+  //Задача № 2
+  function debounceDecoratorNew(func, delay) {
+    let timeoutId;
+    let isTrottled = false;
+
+    function wrapper(...args) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func(args);
+        wrapper.count++;
+      }, delay);
+
+      if (!isTrottled) {
+        func(...args);
+        wrapper.count++;
+        isTrottled = true;
+      }
+      
+      wrapper.allCount++;
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
+  }
